cfa4b6933d95fde5cfcdb090f00047e947df8661
kulvinder kakar <kulvinderkakar14@gmail.com>
2021-08-02 16:34:27 +1000

    verify authorization headers with jwt

---
+++ util/check-auth.js
@@ -0,0 +1,22 @@
+const {AuthenticationError} =require('apollo-server')
+const jwt=require('jsonwebtoken')
+const { JWT_SECRET } = require('../config')
+
+module.export=(context)=> {
+    const authHeader = context.req.headers.authorization
+    if(authHeader) {
+        const token = authHeader.split('Bearer ')[1]
+        if (token) {
+            try {
+                const user =jwt.verify(token, JWT_SECRET);
+                return user
+            } catch(err) {
+                throw new AuthenticationError("Invalid Token")
+            }
+        } else {
+            throw new AuthenticationError("Authorization Token msut be Bearer [token]")
+        }
+    } else {
+        throw new AuthenticationError("Authorization Token not provided")
+    }
+}
