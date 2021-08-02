a28e541e87eae77b0743673b09e6ca64b1d89426
kulvinder kakar <kulvinderkakar14@gmail.com>
2021-08-02 17:11:19 +1000

    resovlers for comments

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,54 @@
+const Post = require('../../models/Post')
+const checkAuth= require("../../util/check-auth")
+const {AuthenticationError, UserInputError} = require('apollo-server')
+
+module.exports = {
+    Mutation: {
+        async createComment(_,{postId, body}, context) {
+            const user=checkAuth(context)
+            const post = Post.findById(postId)
+            if (body.trim()==="") {
+                throw new UserInputError("Body field should not be empty", {
+                    errors: {
+                        body:"Body fiels should not be empty"
+                    }
+                })
+            }
+            if(post) {
+                post.comments.unshift({
+                    username:user.username,
+                    body,
+                    createdAt:new Date().toISOString()
+                })
+                await post.save()
+                return post
+            } else {
+                throw new UserInputError("Post not found", {
+                    errors: {
+                        message:"post not found"
+                    }
+                })
+            }
+        }
+    },
+    async deleteComment(_,{postId, commentId},context) {
+        const user= checkAuth(context);
+        const post = Post.findById(postId);
+        if(post){
+            const commentIndex= post.comments.findIndex((c)=>c.id==commentId)
+            if(post.comments[commentIndex].username === user.username) {
+                post.comments.splice(commentIndex,1)
+                await post.save()
+                return post
+            } else {
+                throw new AuthenticationError("Not allowed to delete post")
+            }
+        } else {
+            throw new UserInputError("Post not found", {
+                errors: {
+                    message:" Post not found"
+                }
+            })
+        }
+    }
+}
